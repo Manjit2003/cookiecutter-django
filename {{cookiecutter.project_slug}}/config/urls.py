a3cafbd5e3a0a60d01f6aa2e785cfa2b956d70eb
Manjit Pardeshi <manjitpardeshi91991@gmail.com>
2022-08-05 11:58:27 +0530

    fix: fixed tests

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -1,15 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 {%- if cookiecutter.use_async == 'y' %}
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 {%- endif %}
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 {%- if cookiecutter.use_drf == 'y' %}
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.authtoken.views import obtain_auth_token
 {%- endif %}
 
 urlpatterns = [
